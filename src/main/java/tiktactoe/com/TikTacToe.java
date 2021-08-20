8681cd80ce6b3b4fc0a164cffc28372cd59c69bd
nandinibaride <nandinibaride1234@gmail.com>
2021-08-20 14:18:53 +0530

    Added: Create Empty Board

--- src/main/java/tiktactoe/com/TikTacToe.java
+++ src/main/java/tiktactoe/com/TikTacToe.java
@@ -1,14 +1,20 @@
 package tiktactoe.com;
 
 public class TikTacToe {
-	public static void main(String args[]) {
-		
-		char arr[] = new char[10];  // create array
-		for (int i=1;i<arr.length;i++) {   // using for loop
-				arr[i] = '-';             
+	/*
+	 * @purpose: Create a Board and initialize blank space
+	 * @return: No return value  
+	 */
+	public static void createBoard(){      
+		char arr[] = new char[10];          
+		for (int i=1;i<arr.length;i++) {   
+				arr[i] = ' ';             
 				System.out.println(arr[i]);
 		}
-		
+	}
+	
+	public static void main(String args[]) {
+			createBoard();
 	}
 
 }
