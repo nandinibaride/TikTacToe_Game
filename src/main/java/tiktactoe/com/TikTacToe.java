a06b311e8755fb5e16ab36a888ff83c34d5d09af
nandinibaride <nandinibaride1234@gmail.com>
2021-08-20 10:14:01 +0530

    Added: TikTacToe uc-1 part creating board

--- src/main/java/tiktactoe/com/TikTacToe.java
+++ src/main/java/tiktactoe/com/TikTacToe.java
@@ -3,8 +3,8 @@ package tiktactoe.com;
 public class TikTacToe {
 	public static void main(String args[]) {
 		
-		char arr[] = new char[9];  // create array
-		for (int i=0;i<arr.length;i++) {   // using for loop
+		char arr[] = new char[10];  // create array
+		for (int i=1;i<arr.length;i++) {   // using for loop
 				arr[i] = '-';             
 				System.out.println(arr[i]);
 		}
